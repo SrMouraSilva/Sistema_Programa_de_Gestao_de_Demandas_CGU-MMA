0c7c9ece0569d63ee5f53f4e84af922ff4fe1405
@fabrica.fab <fabrica.fab@basis.com.br>
2020-08-19 11:58:57 -0300

    Fix(MMA202017-867): Alateração base para add coluna hirarquia na tabela unidade; add relacionamentos

--- PGD.Domain/Entities/RH/Unidade.cs
+++ PGD.Domain/Entities/RH/Unidade.cs
@@ -8,9 +8,11 @@ namespace PGD.Domain.Entities.RH
         public int IdUnidade { get; set; }
         public string Nome { get; set; }
         public string Codigo { get; set; }
+        public string Hierarquia { get; set; }
         #endregion
         public string Sigla { get; set; }
         public bool Excluido { get; set; }
         public virtual ICollection<UsuarioPerfilUnidade> UsuariosPerfisUnidades { get; set; }
+        public virtual ICollection<Unidade_TipoPacto> UnidadesTiposPactos { get; set; }
     }
 }
--- PGD.Domain/Entities/Unidade_TipoPacto.cs
+++ PGD.Domain/Entities/Unidade_TipoPacto.cs
@@ -1,11 +1,5 @@
 ﻿using DomainValidation.Validation;
 using PGD.Domain.Entities.RH;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PGD.Domain.Entities
 {
@@ -16,12 +10,13 @@ namespace PGD.Domain.Entities
             ValidationResult = new ValidationResult();
         }
         public int IdUnidade_TipoPacto { get; set; }        
-        public int IdUnidade { get; set; }        
-        [ForeignKey("TipoPacto")]
+        public int IdUnidade { get; set; } 
         public int IdTipoPacto { get; set; }
         public virtual TipoPacto TipoPacto { get; set; }
         public bool IndPermitePactoExterior { get; set; }        
         public ValidationResult ValidationResult { get; set; }
+        public virtual TipoPacto Tipo { get; set; }
+        public virtual Unidade Unidade { get; set; }
         public bool IsValid()
         {
             return true;
